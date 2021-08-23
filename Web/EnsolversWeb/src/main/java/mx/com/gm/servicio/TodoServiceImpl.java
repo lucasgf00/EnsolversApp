42d813b24729e3cf90afc38f8125a813a4f6fa4f
lucas <lucasgonzalezfalcon@gmail.com>
2021-08-23 20:39:10 -0300

    Phase 1 finished with a Login and a Responsive design

---
+++ Web/EnsolversWeb/src/main/java/mx/com/gm/servicio/TodoServiceImpl.java
@@ -0,0 +1,41 @@
+package mx.com.gm.servicio;
+
+import java.util.List;
+import mx.com.gm.dao.TodoDao;
+import mx.com.gm.domain.Todo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service 
+public class TodoServiceImpl implements TodoService {
+
+    @Autowired
+    private TodoDao todoDao;
+    
+    @Override
+    @Transactional(readOnly = true)
+    public List<Todo> listarTodo() {
+        return (List<Todo>) todoDao.findAll();
+    }
+
+    @Override
+    @Transactional
+    public void guardar(Todo todo) {
+        todoDao.save(todo);
+    }
+
+    @Override
+    @Transactional
+    public void eliminar(Todo todo) {
+        todoDao.delete(todo);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Todo encontrarTodo(Todo todo) {
+        return todoDao.findById(todo.getIdTodo()).orElse(null);
+    }
+    
+    
+}
