68cc45fa9c2306107741ad6f5f52e53b414c8256
lucas <lucasgonzalezfalcon@gmail.com>
2021-08-25 15:17:11 -0300

    This is the final version of the webapp

--- Web/EnsolversWeb/src/main/java/mx/com/gm/dao/FolderDao.java
+++
@@ -1,8 +0,0 @@
-package mx.com.gm.dao;
-
-import mx.com.gm.domain.Folder;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FolderDao extends JpaRepository<Folder, Long>{
-
-}
--- Web/EnsolversWeb/src/main/java/mx/com/gm/dao/TodoDao.java
+++
@@ -1,8 +0,0 @@
-package mx.com.gm.dao;
-
-import mx.com.gm.domain.Todo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoDao extends JpaRepository<Todo, Long>{
-    
-}
